821c066ef766aa9ddbb71b31f54ee9c2d9f3e595
gabriel-dev-96 <gabriel1996ca@gmail.com>
2020-11-11 15:32:56 -0500

    otro comm

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -39,7 +39,7 @@ const About = () => {
     return (
         <div className="about">
             <h6 className="about__intro">
-                Hola! Mi nombre Gabriel, este es mi sitio web esta construido en Reactjs, subido en mi servidor free de heroku, se utilizo rutas, comunicacion de componentes y el uso de hooks, en este sitio web, el modulo de about como el de resume, tiene datos de relleno sacados, de lorem ipsum, pero el modulo de projects, tiene mis proyectos enlazados a su repositorio de github
+                Hola! Mi nombre Gabriel, este es mi sitio web esta construido en Reactjs, subido en mi servidor free de heroku, se utilizo rutas, comunicacion de componentes y el uso de hooks, en este sitio web, el modulo de about como el de resume, tiene datos de relleno sacados, de lorem ipsum, pero el modulo de projects, tiene mis proyectos personales , su sitio web como enlazados a su repositorio de github.
             </h6>
             <div className="container about__container">
                 <h6 className="about__heading">What I offer</h6>
